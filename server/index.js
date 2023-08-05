2662771cc7f6cfa0f1dc66d0ba1639cb75970a0d
Ramandeep <ramank6238299@gmail.com>
2023-08-05 14:47:33 +0530

    CORS error

--- server/index.js
+++ server/index.js
@@ -8,6 +8,17 @@ const app = express();
 const socket = require("socket.io");
 require("dotenv").config();
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "Content-Type",
+    "Authorization"
+  );
+  next();
+});
+
 app.use(cors());
 app.use(express.json());
 
